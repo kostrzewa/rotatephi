80f327e0060eafb56d8332c62b87e362e1f6c01b
Bartosz Kostrzewa <bartosz.kostrzewa@desy.de>
2016-05-13 10:47:24 +0200

    add convenience shell script to carry out rotation of a scalar ensemble

---
+++ dorotate.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+rotatephi=/hadron/bartek/code/rotatephi/rotatephi
+
+if [ -z ${1} -o -z ${2} ]; then
+  echo "usage: ./dorotate.sh <idir> <odir> <logdir>"
+  echo "A logfile will be written to <logdir>, with its name derived from the basename of <odir>"
+  exit 1
+fi
+
+if [ ! -d ${1} -o ! -d ${2} -o ! -d ${3} ]; then
+  echo "one of the specified directories does not seem to exist!"
+  exit 2
+fi
+
+logfile=${3}/$( basename ${2} ).log
+
+echo $logfile
+
+date > ${logfile}
+
+for i in ${1}/*.dat; do
+  # extract scalar configuration number
+  cnum=$( basename $i | awk --field-separator '.' '{print $(NF-1)}' | awk --field-separator '_' '{print $NF}' )
+  ${rotatephi} -i ${i} -o ${2}/scalar.$cnum -L 24 -T 48 | tee -a ${logfile}
+done
+
+date >> ${logfile}
